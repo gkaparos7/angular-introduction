514d3f5e8f4f01143d44d4470a43bfc36e467ee0
gkaparos <gkaparos@hotmail.com>
2023-10-12 21:06:19 +0300

    fasfasf

--- angular-introduction/src/app/app.component.ts
+++ angular-introduction/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PersonComponent } from './person/person.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PersonComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   name : string = "Giannis";
   lastname = "Kaparos";
+
+  person = {
+    givenName: "Giannis",
+    surName: "Kaparos",
+    age: 0x37,
+    email: "gkaparos@aueb.gr",
+  };
 }
